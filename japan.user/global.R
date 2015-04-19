5e64d919baa4d579aae9d688945635d8108850e7
Shinya Uryu <suika1127@gmail.com>
2015-04-19 18:33:42 +0900

    [FIX]for deploy

--- japan.user/global.R
+++ japan.user/global.R
@@ -1,8 +1,12 @@
 # Japan.useR app.
-library(shiny); library(shinydashboard)
+library(shiny)
+require(rmarkdown)
+library(shinydashboard)
 library(dplyr)
-library(leaflet)
-library(Japan.useR)
-data("JRSlide"); data("JREvent")
+library(RCurl)
+# install_github("uribo/Japan.useR"); library(Japan.useR)
+JRSlide <- read.csv(text = getURL("https://raw.githubusercontent.com/uribo/Japan.useR/master/inst/assets/raw_data/JapanRSlide.csv"), header = TRUE)
+JREVent <- read.csv(text = getURL("https://raw.githubusercontent.com/uribo/Japan.useR/master/inst/assets/raw_data/JapanREvent.csv"), header = TRUE)
+# data("JRSlide"); data("JREvent")
 
 JRSlide.browse <- JRSlide %>% filter(!is.na(Slide)) %>% droplevels()
